48134c11d57471de2a2dab848fd76fd90b406cf4
Aristotelis Sibetheros <50674959+Aristotelis-Sib@users.noreply.github.com>
2020-02-02 19:17:37 +0200

    Add files via upload
    Testing upload from github

--- git_commit.py
+++ git_commit.py
@@ -2,3 +2,10 @@ print("THIS IS a git commit")
 print("I made some changes ")
 
 print("This i a change")
+
+print("this is only in dev branch")
+
+print("Bracnh of bra   nch stuff")
+
+
+print("TEST ON UPLOAD")
